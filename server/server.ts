8f60235671277b320ebe55b4e00fea29a86e8f8c
lam-vincent <mrccniv@gmail.com>
2023-11-07 18:16:17 +0100

    chore: :broom:
    Co-authored-by: 16Noct <xvinoctis@pm.me>

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,6 @@
 import express, { NextFunction, Response } from "express";
 import cors from "cors";
 import mysql from "mysql";
-import { pool } from "./config/database";
 import authRoutes from "./routes/authRoutes";
 import dashboardRoutes from "./routes/dashboardRoutes";
 import findRoutes from "./routes/findRoutes";
@@ -9,20 +8,11 @@ import groupRoutes from "./routes/groupRoutes";
 import reserveRoutes from "./routes/reserveRoutes";
 import { authMiddleware } from "./middlewares/authMiddleware";
 
-interface CustomRequest extends express.Request {
-  pool: mysql.Pool;
-}
-
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-
-// app.use((req: CustomRequest, res: Response, next: NextFunction) => {
-//   req.pool = pool;
-//   next();
-// });
 
 app.use("/server/auth", authRoutes);
 
