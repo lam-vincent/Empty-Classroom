bbe8b538c9d3b1a6101a2a25601b15bfcfecec3b
lam-vincent <mrccniv@gmail.com>
2023-11-07 18:11:39 +0100

    fix: reserveRoutes are working
    Co-authored-by: 16Noct <xvinoctis@pm.me>

--- server/services/reserveService.ts
+++ server/services/reserveService.ts
@@ -1,12 +1,14 @@
 import { pool } from "../config/database";
 
 const createReservation = async (reservationData: any) => {
-  const query = "INSERT INTO Reserve(id_room, id_user) VALUES(?, ?)";
+  const query =
+    "INSERT INTO Reserve(id_room, id_user, Title, Description, Start_Time, End_Time) VALUES(?, ?, ?, ?, ?, ?)";
   await pool.query(
     query,
-    [reservationData.room_id, reservationData.user_id],
+    Object.values(reservationData),
     (error, results, fields) => {
       if (error) throw error;
+      console.log(results);
       return results;
     }
   );
@@ -14,24 +16,32 @@ const createReservation = async (reservationData: any) => {
 
 const fetchReservationByUser = async (userId: string) => {
   const query = "SELECT * FROM Reserve WHERE id_user = ?";
-  const result = await pool.query(query, [userId], (error, results, fields) => {
+  await pool.query(query, [userId], (error, results, fields) => {
     if (error) throw error;
+    console.log(results);
     return results;
   });
 };
 
-const updateReservation = async (roomId: string, userId: string) => {
-  const query = "UPDATE Reserve WHERE id_room = ? AND id_user = ?";
-  await pool.query(query, [roomId, userId], (error, results, fields) => {
-    if (error) throw error;
-    return results;
-  });
+const updateReservation = async (id_reserve: string, reservationData: any) => {
+  const query =
+    "UPDATE Reserve SET id_room = ?, id_user = ?, Title = ?, Description = ?, Start_Time = ?, End_Time = ? WHERE id_reserve = ?";
+  await pool.query(
+    query,
+    [...Object.values(reservationData), id_reserve],
+    (error, results, fields) => {
+      if (error) throw error;
+      console.log(results);
+      return results;
+    }
+  );
 };
 
-const deleteReservation = async (roomId: string, userId: string) => {
-  const query = "DELETE FROM Reserve WHERE id_room = ? AND id_user = ?";
-  await pool.query(query, [roomId, userId], (error, results, fields) => {
+const deleteReservation = async (id: string) => {
+  const query = "DELETE FROM Reserve WHERE id_reserve = ?";
+  await pool.query(query, [id], (error, results, fields) => {
     if (error) throw error;
+    console.log(results);
     return results;
   });
 };
